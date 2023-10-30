07a4d9fe40efd2d92d4c56bb89b47a85dd559288
Huy214 <101809224+Carliscoding@users.noreply.github.com>
2023-10-30 23:40:46 +0700

    Delete lab2 directory

--- lab2/lab2_bai3/app/src/main/java/com/example/lab2_bai3/EmployeeFullTime.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab2_bai3;
-
-public class EmployeeFullTime extends Employee {
-    public double tinhLuong() {return 500;}
-    @Override
-    public String toString() {
-        return  super.toString() +"--"+"->FullTime="+tinhLuong();
-    }
-}
